325142ccfa07d93f01e4ece583b6050fb7458b22
johanmora923 <johanmora923@gmail.com>
2025-03-07 14:05:15 -0400

    Enhance socket connection in LiveChat component to use websocket transport and enable credentials

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -8,7 +8,10 @@ import { AiOutlineRollback } from "react-icons/ai";
 import { FaMicrophone } from "react-icons/fa6";
 import CryptoJS from 'crypto-js';
 
-const socket = io('https://backend-auction-sigma.vercel.app/connection'); 
+const socket = io('https://backend-auction-sigma.vercel.app', {
+    transports: ['websocket'], 
+    withCredentials: true 
+});
 
 const LiveChat = ({ sender_id }) => {
     const [contacts, setContacts] = useState([]);
